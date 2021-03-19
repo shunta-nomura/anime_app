ecb2bb071fbccef7c24735b5af76a18c383b17ee
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2021-03-19 20:18:16 +0900

    gem install

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,21 +13,24 @@ class BlogsController < ApplicationController
     searchWorks(
       seasons: ["2017-spring"],
       orderBy: { field: WATCHERS_COUNT, direction: DESC },
-      first: 3
+      first: 5
     ) {
       edges {
         node {
           title
           watchersCount
+          image {
+            recommendedImageUrl
+          }
         }
       }
     }
   }
-  
     GRAPHQL
   def index
     @works = result.data.viewer.name
-    binding.pry
+    @animes = animeresult.data.search_works.edges
+    # binding.pry
   end
 
   private
